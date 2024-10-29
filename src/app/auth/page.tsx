7cca94d6b01a8456a83be243023ee8b0fa6680e2
RahulB <rahulbisht1012@gmail.com>
2024-10-29 18:03:15 +0530

    first commit

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import {useEffect} from "react";
+import Image from "next/image";
+import {useRouter} from "next/navigation";
+import GoogleLogo from "../../../public/images/google.png";
+import GithubLogo from "../../../public/images/github.png";
+
+import {createClient} from "../../utils/supabase/client";
+import type {User, SupabaseClient} from "@supabase/supabase-js";
+
+const Auth = () => {
+    const supabase: SupabaseClient = createClient();
+    const router = useRouter();
+
+    useEffect(() => {
+        const fetchUser = async (): Promise<void> => {
+            const {data}: {data: {user: User | null}} = await supabase.auth.getUser();
+            if (data && data.user) {
+                router.push("/"); // Use Next.js router to redirect
+            }
+        };
+        fetchUser();
+    }, [router, supabase]);
+
+    const handleGoogleAuth = async (): Promise<void> => {
+        await supabase.auth.signInWithOAuth({
+            provider: "google",
+            options: {
+                redirectTo: `${location.origin}/auth/callback`,
+            },
+        });
+    };
+    const handleGithubAuth = async (): Promise<void> => {
+        await supabase.auth.signInWithOAuth({
+            provider: "github",
+            options: {
+                redirectTo: `${location.origin}/auth/callback`,
+            },
+        });
+    };
+
+    return (
+        <>
+            <section className="bg-black text-white items-center justify-center flex gap-14 flex-col w-full min-h-screen font-outfit">
+                <header>
+                    <h1 className="text-3xl">Howdy👋 from analytiX</h1>
+                </header>
+
+                <main>
+                    <div className="flex gap-4">
+                        <button
+                            onClick={handleGoogleAuth}
+                            className="button flex gap-3 items-center justify-center"
+                            aria-label="Sign in with Google"
+                        >
+                            <Image src={GoogleLogo} alt="Google Logo" width={25} height={25} />
+                            <span>Signin with Google</span>
+                        </button>
+
+                        <button
+                            onClick={handleGithubAuth}
+                            className="button flex gap-3 items-center justify-center"
+                            aria-label="Sign in with Github"
+                        >
+                            <Image src={GithubLogo} alt="Github Logo" width={25} height={25} />
+                            <span>Signin with Github</span>
+                        </button>
+                    </div>
+                </main>
+            </section>
+        </>
+    );
+};
+
+export default Auth;
