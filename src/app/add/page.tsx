d7e0675b8e568d46db9878c8e99077e4b48c5915
RahulB <rahulbisht1012@gmail.com>
2024-11-02 20:47:27 +0530

    Modified /add/page.tsx
    Added a "back to  dashboard " button which will take the user back to
    the main dashboard of the app which contains all the website on which
    you want to analyze traffic.

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -9,7 +9,7 @@ import type {User} from "@supabase/supabase-js";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import {irBlack} from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-import {ArrowRight, Copy, Check} from "lucide-react";
+import {ArrowRight, Copy, Check, ArrowLeft} from "lucide-react";
 
 const AddPage = () => {
     const [step, setStep] = useState<number>(1);
@@ -122,110 +122,123 @@ const AddPage = () => {
 
     return (
         <>
-            <main className="font-Lexend bg-black w-full min-h-screen flex flex-col items-center justify-center">
-                {/* logo of the website */}
-                <h1 className="text-white/80 text-2xl sm:text-4xl tracking-wider">.analytiX</h1>
+            <main className="font-Lexend bg-black">
+                <button
+                    onClick={() => router.push("/dashboard")}
+                    className="text-white/50 absolute top-5 left-4 flex gap-1 items-center group hover:text-white/90"
+                >
+                    <ArrowLeft
+                        size={18}
+                        className="transform transition-transform duration-300 group-hover:-translate-x-1"
+                    />
+                    <span className="text-sm sm:text-base">dashboard</span>
+                </button>
 
-                <div className="flex flex-col items-center justify-center p-12 w-full z-0 border-y border-white/5 bg-black text-white mt-10">
-                    {step === 1 ? (
-                        <>
-                            <div className="w-full flex flex-col items-center justify-center space-y-10">
-                                <span className="w-full lg:w-[50%] group">
-                                    <p className="text-white/40 pb-4 group-hover:text-white smooth">Domain Name</p>
-                                    <input
-                                        type="text"
-                                        placeholder="enter the domain name"
-                                        className="input"
-                                        aria-label="Input field for domain name input"
-                                        value={website}
-                                        onChange={(e) => handleWebsiteChange(e)}
-                                    />
-                                    {error ? (
-                                        <p className="text-xs text-red-500 pt-2 font-normal">{error}</p>
-                                    ) : (
-                                        <p className="text-xs text-white/20 pt-2 font-normal">
-                                            enter the domain / subdomain without {"www / http"}
-                                        </p>
-                                    )}
-                                </span>
-                                {error === "" && (
-                                    <button
-                                        onClick={checkDomainAddedBefore}
-                                        className={`button flex items-center gap-1${
-                                            loading || check ? "cursor-not-allowed !bg-[#0c0c0c]" : ""
-                                        }`}
-                                        disabled={loading}
-                                    >
-                                        {loading || check ? (
-                                            <>
-                                                <svg
-                                                    className="w-5 h-5 mr-2 animate-spin"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    fill="none"
-                                                    viewBox="0 0 24 24"
-                                                >
-                                                    <circle
-                                                        className="opacity-25"
-                                                        cx="12"
-                                                        cy="12"
-                                                        r="10"
-                                                        stroke="currentColor"
-                                                        strokeWidth="4"
-                                                    ></circle>
-                                                    <path
-                                                        className="opacity-75"
-                                                        fill="currentColor"
-                                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                                    ></path>
-                                                </svg>
-                                                {loading ? <span>adding. . .</span> : <span>checking. . .</span>}
-                                            </>
+                <section className="w-full min-h-screen flex flex-col items-center justify-center">
+                    {/* logo of the website */}
+                    <h1 className="text-white/80 text-2xl sm:text-4xl tracking-wider">.analytiX</h1>
+
+                    <div className="flex flex-col items-center justify-center p-12 w-full z-0 border-y border-white/5 bg-black text-white mt-10">
+                        {step === 1 ? (
+                            <>
+                                <div className="w-full flex flex-col items-center justify-center space-y-10">
+                                    <span className="w-full lg:w-[50%] group">
+                                        <p className="text-white/40 pb-4 group-hover:text-white smooth">Domain Name</p>
+                                        <input
+                                            type="text"
+                                            placeholder="enter the domain name"
+                                            className="input"
+                                            aria-label="Input field for domain name input"
+                                            value={website}
+                                            onChange={(e) => handleWebsiteChange(e)}
+                                        />
+                                        {error ? (
+                                            <p className="text-xs text-red-500 pt-2 font-normal">{error}</p>
                                         ) : (
-                                            <span>add website</span>
+                                            <p className="text-xs text-white/20 pt-2 font-normal">
+                                                enter the domain / subdomain without {"www / http"}
+                                            </p>
                                         )}
-                                    </button>
-                                )}
-                            </div>
-                        </>
-                    ) : (
-                        <>
-                            <div className="w-full items-center justify-center flex flex-col space-y-10">
-                                <span className="w-full lg:w-[50%] relative">
+                                    </span>
+                                    {error === "" && (
+                                        <button
+                                            onClick={checkDomainAddedBefore}
+                                            className={`button flex items-center gap-1${
+                                                loading || check ? "cursor-not-allowed !bg-[#0c0c0c]" : ""
+                                            }`}
+                                            disabled={loading}
+                                        >
+                                            {loading || check ? (
+                                                <>
+                                                    <svg
+                                                        className="w-5 h-5 mr-2 animate-spin"
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none"
+                                                        viewBox="0 0 24 24"
+                                                    >
+                                                        <circle
+                                                            className="opacity-25"
+                                                            cx="12"
+                                                            cy="12"
+                                                            r="10"
+                                                            stroke="currentColor"
+                                                            strokeWidth="4"
+                                                        ></circle>
+                                                        <path
+                                                            className="opacity-75"
+                                                            fill="currentColor"
+                                                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                                        ></path>
+                                                    </svg>
+                                                    {loading ? <span>adding. . .</span> : <span>checking. . .</span>}
+                                                </>
+                                            ) : (
+                                                <span>add website</span>
+                                            )}
+                                        </button>
+                                    )}
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                                <div className="w-full items-center justify-center flex flex-col space-y-10">
+                                    <span className="w-full lg:w-[50%] relative">
+                                        <button
+                                            onClick={handleCopy}
+                                            className="button absolute top-2 right-2 bg-transparent text-white !p-2 rounded focus:outline-none"
+                                            aria-label="Copy code to clipboard"
+                                        >
+                                            {isCopied ? <Check size={20} color="#1bee34" /> : <Copy size={20} />}
+                                        </button>
+                                        <SyntaxHighlighter
+                                            language="javascript"
+                                            style={irBlack}
+                                            className="rounded-lg border border-white/10 !text-sm"
+                                        >
+                                            {codeString}
+                                        </SyntaxHighlighter>
+
+                                        <p className="text-xs text-white/30 font-light pt-2">
+                                            paste this snippet in the{" "}
+                                            <strong className="text-red-600">{"<head>"}</strong> tag of the your app.
+                                        </p>
+                                    </span>
+
                                     <button
-                                        onClick={handleCopy}
-                                        className="button absolute top-2 right-2 bg-transparent text-white !p-2 rounded focus:outline-none"
-                                        aria-label="Copy code to clipboard"
+                                        onClick={() => router.push(`/w/${website.trim()}`)}
+                                        className="button flex gap-2 items-center group"
                                     >
-                                        {isCopied ? <Check size={20} color="#1bee34" /> : <Copy size={20} />}
+                                        <span>Check insights</span>
+                                        <ArrowRight
+                                            size={20}
+                                            className="transform transition-transform duration-200 group-hover:translate-x-1"
+                                        />
                                     </button>
-                                    <SyntaxHighlighter
-                                        language="javascript"
-                                        style={irBlack}
-                                        className="rounded-lg border border-white/10 !text-sm"
-                                    >
-                                        {codeString}
-                                    </SyntaxHighlighter>
-
-                                    <p className="text-xs text-white/30 font-light pt-2">
-                                        paste this snippet in the <strong className="text-red-600">{"<head>"}</strong>{" "}
-                                        tag of the your app.
-                                    </p>
-                                </span>
-
-                                <button
-                                    onClick={() => router.push(`/w/${website.trim()}`)}
-                                    className="button flex gap-2 items-center group"
-                                >
-                                    <span>Check insights</span>
-                                    <ArrowRight
-                                        size={20}
-                                        className="transform transition-transform duration-200 group-hover:translate-x-1"
-                                    />
-                                </button>
-                            </div>
-                        </>
-                    )}
-                </div>
+                                </div>
+                            </>
+                        )}
+                    </div>
+                </section>
             </main>
         </>
     );
