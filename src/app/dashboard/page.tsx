a1a75ed3a8956e824634bba63d9e9d12b7e13976
RahulB <rahulbisht1012@gmail.com>
2024-11-21 18:29:07 +0530

    Made the Websitescomponent modular.
    With the principles of separation of concertions, we separated the
    Websites component from the dashboard main component. This will help us
    in implementing the future pagination.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,55 +1,20 @@
 "use client";
-import {useEffect, useState} from "react";
 import {redirect} from "next/navigation";
 import Link from "next/link";
 import Header from "../_components/Header";
 import useUser from "../../hooks/useUser";
-import {supabase} from "@/config/SUPABASE_CLIENT";
 import type {User} from "@supabase/supabase-js";
-
-// Define the interface for the website object
-interface Website {
-    id: string; // Assuming 'id' is a string, change to number if necessary
-    created_at: string; // Change the type based on your database schema
-    website_name: string;
-    user_id: string; // Assuming user_id is also a string
-}
+import Websites from "./websites";
+import {useEffect} from "react";
 
 const Dashboard = () => {
     const [user] = useUser();
     // Assert that user is of type User
     const currentUser = user as User;
-
-    const [loading, setLoading] = useState<boolean>(true);
-    const [websites, setWebsites] = useState<Website[]>([]);
-
     useEffect(() => {
         if (!user) return;
         if (user === "no user") redirect("/auth");
-
-        fetchWebsitesOfUser();
     }, [user]);
-
-    const fetchWebsitesOfUser = async () => {
-        const {data, error} = await supabase
-        .from("websites")
-        .select()
-        .eq("user_id", currentUser?.id)
-        .order("created_at", {ascending: false});
-
-        // alert(data);
-        console.log(data);
-
-        if (error) {
-            console.error(error.message);
-            alert("Some error occured!");
-        }
-
-        if (data) {
-            setLoading(false);
-            setWebsites(data);
-        }
-    };
 
     return (
         <>
@@ -63,23 +28,7 @@ const Dashboard = () => {
                             <button className="button">+ add website</button>
                         </Link>
                     </div>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-full gap-10 p-6 z-40">
-                        {/* Map all the websites here */}
-                        {loading ? (
-                            <p className="text-white">Wait while we fetch the data</p>
-                        ) : (
-                            websites &&
-                            websites.map((website: any, index: Number) => {
-                                return (
-                                    <Link href={`/w/${website?.website_name}`} key={website.id + "#"}>
-                                        <div className="border border-white/5 rounded-md py-12 px-6 text-white bg-black w-full cursor-pointer smooth hover:border-white/20 hover:bg-[#050505]">
-                                            <h2>{website?.website_name}</h2>
-                                        </div>
-                                    </Link>
-                                );
-                            })
-                        )}
-                    </div>
+                    <Websites currentUserId={currentUser.id} />
                 </div>
             </main>
         </>
---
+++ src/app/dashboard/websites.tsx
@@ -0,0 +1,69 @@
+"use client";
+import Link from "next/link";
+import {useEffect, useState} from "react";
+import {supabase} from "@/config/SUPABASE_CLIENT";
+
+// Define the interface for the website object
+interface Website {
+    id: string; // Assuming 'id' is a string, change to number if necessary
+    created_at: string; // Change the type based on your database schema
+    website_name: string;
+    user_id: string; // Assuming user_id is also a string
+}
+interface WebsitesProps {
+    currentUserId: string; // ID of the authenticated user
+}
+
+const Websites = ({currentUserId}: WebsitesProps) => {
+    const [loading, setLoading] = useState<boolean>(true);
+    const [websites, setWebsites] = useState<Website[]>([]);
+
+    useEffect(() => {
+        fetchWebsitesOfUser();
+    }, [websites, currentUserId]);
+
+    const fetchWebsitesOfUser = async () => {
+        const {data, error} = await supabase
+        .from("websites")
+        .select()
+        .eq("user_id", currentUserId)
+        .order("created_at", {ascending: false});
+
+        // alert(data);
+        console.log(data);
+
+        if (error) {
+            console.error(error.message);
+            alert("Some error occured!");
+        }
+
+        if (data) {
+            setLoading(false);
+            setWebsites(data);
+        }
+    };
+
+    return (
+        <>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-full gap-10 p-6 z-40">
+                {/* Map all the websites here */}
+                {loading ? (
+                    <p className="text-white">Wait while we fetch the data</p>
+                ) : (
+                    websites &&
+                    websites.map((website: any, index: Number) => {
+                        return (
+                            <Link href={`/w/${website?.website_name}`} key={website.id + "#"}>
+                                <div className="border border-white/5 rounded-md py-12 px-6 text-white bg-black w-full cursor-pointer smooth hover:border-white/20 hover:bg-[#050505]">
+                                    <h2>{website?.website_name}</h2>
+                                </div>
+                            </Link>
+                        );
+                    })
+                )}
+            </div>
+        </>
+    );
+};
+
+export default Websites;
