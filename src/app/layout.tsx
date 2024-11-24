223a2360ffbe7e0c5fd83a09985bf9831e5593e9
RahulB <rahulbisht1012@gmail.com>
2024-11-25 01:22:53 +0530

    SEO improvement meta tags in the layout ✅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 
 import {Lexend, Outfit} from "next/font/google";
 import {UserAuthContextProvider} from "../context/UserContext";
+import Image from "next/image";
 const lexend = Lexend({
     subsets: ["latin"],
     weight: ["100", "200", "300", "400", "500", "600", "700"],
@@ -15,8 +16,46 @@ const outfit = Outfit({
 });
 
 export const metadata: Metadata = {
-    title: "WebTrack",
-    description: "Get your website analytics in just few clicks",
+    title: "WebTrack - Website Analytics Tool",
+    description:
+        "Track your website's performance and insights with WebTrack. Get detailed analytics, user behavior reports, and traffic analysis in just a few clicks.",
+    keywords: [
+        "website analytics",
+        "free website analytics tool",
+        "free analytics dashboard for developers",
+        "easy-to-use web analytics",
+        "website traffic analysis",
+        "analytics dashboard",
+
+        "user behavior tracking",
+        "simple website tracker",
+        "website performance analyzer",
+        "site performance monitoring",
+    ],
+    // Open Graph (OG) Tags
+    openGraph: {
+        title: "WebTrack - Website Analytics Tool",
+        description:
+            "Track your website's performance and insights with WebTrack. Get detailed analytics, user behavior reports, and traffic analysis in just a few clicks.",
+        url: "https://webtrack.vercel.app", // Replace with your actual URL
+        images: [
+            {
+                url: "https://res.cloudinary.com/dthrirmoj/image/upload/c_thumb,w_200,g_face/v1732476248/Screenshot_2024-11-25_005124_idy4tk.png", // Replace with your preview image URL
+                width: 1200,
+                height: 630,
+                alt: "WebTrack Preview Image", // Alt text for the image
+            },
+        ],
+        siteName: "WebTrack",
+    },
+    // Twitter Card Tags
+    twitter: {
+        card: "summary_large_image", // Use 'summary_large_image' for larger images
+        title: "WebTrack - Website Analytics Tool",
+        description:
+            "Track your website's performance and insights with WebTrack. Get detailed analytics, user behavior reports, and traffic analysis in just a few clicks.",
+        images: "https://res.cloudinary.com/dthrirmoj/image/upload/c_thumb,w_200,g_face/v1732476247/Screenshot_2024-11-25_005103_fgo36r.png", // Replace with your image URL
+    },
 };
 
 export default function RootLayout({
